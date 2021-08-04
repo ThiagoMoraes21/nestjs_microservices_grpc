b699cbb3d540d60185776f459246c377c15ef45c
thiagoMoraes21 <thiago4x70@gmail.com>
2021-08-04 20:27:25 -0300

    configured the proto file and the nest cli to compile non-typescript files

--- math-microservice/src/main.ts
+++ math-microservice/src/main.ts
@@ -1,22 +1,20 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 // Create a logger instance
 const logger = new Logger('Main');
 
 async function bootstrap() {
-    const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-        AppModule,
-        {
-            transport: Transport.TCP,
-            options: {
-                host: '127.0.0.1',
-                port: 8877,
-            }
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+        transport: Transport.GRPC,
+        options: {
+          package: 'app',
+          protoPath: join(__dirname, 'app.proto'),
         },
-    );
+      });
     await app.listen();
     logger.log('Microservice is listening...');
 }
