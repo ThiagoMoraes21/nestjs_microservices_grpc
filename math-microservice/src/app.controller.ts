9ef77ac806b1d0bd2144e9563979ed76a6b8c367
thiagoMoraes21 <thiago4x70@gmail.com>
2021-08-04 20:45:43 -0300

    refactore the app controller methods to match the gRPC standards

--- math-microservice/src/app.controller.ts
+++ math-microservice/src/app.controller.ts
@@ -1,6 +1,14 @@
 import { Controller, Logger } from '@nestjs/common';
 import { MathService } from './math.service';
-import { MessagePattern } from "@nestjs/microservices";
+import { GrpcMethod } from "@nestjs/microservices";
+
+interface INumberArray {
+    data: number[];
+}
+
+interface ISumOfNumberArray {
+    sum: number;
+}
 
 @Controller()
 export class AppController {
@@ -8,9 +16,15 @@ export class AppController {
 
     constructor(private readonly mathSevice: MathService) { }
 
-    @MessagePattern('add')
-    async accumulate(data: number[]) {
-        this.logger.log('Adding: ' + data.toString());
-        return this.mathSevice.accumulate(data);
+    /*
+        Hint:
+            If the gRPC service name and method are the same
+            in the controller you don't need to pass any parameter to the 
+            @GrpcMethod(), it will assume as the default names
+    */
+    @GrpcMethod('AppController', 'Accumulate')
+    accumulate(numberArray: INumberArray): ISumOfNumberArray {
+        this.logger.log('Adding: ' + numberArray.data.toString());
+        return { sum: this.mathSevice.accumulate(numberArray.data) };
     }
 }
